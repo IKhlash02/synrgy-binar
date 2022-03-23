badb3c108557bb9696b7b744119a69ee438c0876
Fikri Rahmat Nurhidayat <fikrirnurhidayat@gmail.com>
2022-03-23 21:17:32 +0700

    docs: initial document

---
+++ README.md
@@ -0,0 +1,8 @@
+# Binar: Challenge 04
+
+- Mulai modifikasi file `server/index.js` apabila ingin membuat HTTP server.
+- Mulai modifikasi folder `public` apabila ingin memodifikasi HTML.
+
+# Tips
+
+Just, hack it bro!
---
+++ public/scripts/main.example.js
@@ -0,0 +1,10 @@
+/*
+ * Contoh kode untuk membaca query parameter,
+ * Siapa tau relevan! :)
+ * */
+
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries());
+
+// Coba olah data ini hehe :)
+console.log(params)
